8b950d4856ebb4fe34a33cad43a3665e3554bb42
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-22 11:44:28 +0200

    obtencion de los datos del perfil de usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,13 +8,14 @@ export class AuthService {
   private _idToken: string;
   private _accessToken: string;
   private _expiresAt: number;
+  public userProfile: any;
 
   auth0 = new auth0.WebAuth({
     clientID: 'Gr8r9jDIUQE3sKKZVsKfXkHTXa2jtmTJ',
     domain: 'dev-rvup257i.auth0.com',
     responseType: 'token id_token',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
   constructor(private router: Router) {
@@ -83,6 +84,20 @@ export class AuthService {
     // Check whether the current time is past the
     // access token's expiry time
     return this._accessToken && new Date().getTime() < this._expiresAt;
+  }
+
+  public getProfile(cb): void {
+    if (!this._accessToken) {
+      throw new Error('Access Token must exist to fetch profile');
+    }
+  
+    const self = this;
+    this.auth0.client.userInfo(this._accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
   }
 
 }
